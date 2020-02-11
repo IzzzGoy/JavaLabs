e3b2a67fd5350d9836e246ba0c54ea9ed17ea399
Alex <xzadmoror@list.ru>
2020-02-11 21:01:23 +0300

    11

--- Java 3/src/com/company/Main.java
+++ Java 3/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import com.company.res.Color;
+import com.company.res.Texture;
+
 public class Main {
 
     public static void main(String[] args) {
+        Circle circle = new Circle(10, 20, 5);
+        Rectangle rectangle = new Rectangle(5,3,6,8);
+        Background background = new Background(new Texture("Flash"), new Color("Red"));
+
+        circle.draw();
+        rectangle.draw();
+        background.draw();
 	// write your code here
     }
 }
